319b204f1977652b96271723065036b4d97bacf6
Nicola Cremaschini <nicola.cremaschini@gmail.com>
2021-05-12 17:01:47 +0200

    first commit

---
+++ src/main/java/it/niccrema/service/CalculateRotationService.java
@@ -0,0 +1,40 @@
+package it.niccrema.service;
+
+
+public class CalculateRotationService {
+  public static int shiftedDiff(String first, String second) {
+      int rotations = 0; 
+      boolean validRotation = false;
+      String stringToShift = first;
+      
+      if(first == null || second == null){
+        throw new RuntimeException("Invalid arguments");
+      }
+    
+      if(first.equals(second)){
+        validRotation = true;
+      }else{
+        
+          
+          for(int i=0; i < second.length(); i++){
+        
+          String shiftedString = shift(stringToShift);
+          rotations++;
+
+          if(shiftedString.equals(second)){
+              validRotation = true;
+              break;
+          }
+
+          stringToShift = shiftedString;
+        }
+      }
+  
+      return validRotation ? rotations  : -1;
+  }
+
+  private static String shift(String stringToShift){
+    int lastCharIndex = stringToShift.length()-1;
+    return stringToShift.charAt(lastCharIndex) + stringToShift.substring(0,lastCharIndex);
+  }
+}
---
+++ src/test/java/it/niccrema/RoutesTest.java
@@ -0,0 +1,20 @@
+package it.niccrema;
+
+import org.junit.Test;
+
+import it.niccrema.service.RoutesService;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class RoutesTest {
+    @Test
+    public void spyRoutes() {
+        RoutesService routes = new RoutesService();
+        assertEquals("MNL, TAG, CEB, TAC, BOR",
+          routes.findRoutes(new String[][]{{"MNL", "TAG"}, {"CEB", "TAC"}, {"TAG", "CEB"}, {"TAC", "BOR"}}));
+        assertEquals("Halifax, Montreal, Toronto, Chicago, Winnipeg, Seattle",
+          routes.findRoutes(new String[][]{{"Chicago", "Winnipeg"}, {"Halifax", "Montreal"}, {"Montreal", "Toronto"}, {"Toronto", "Chicago"}, {"Winnipeg", "Seattle"}}));
+
+    }   
+}
